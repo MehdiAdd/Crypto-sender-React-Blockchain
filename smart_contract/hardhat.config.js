be81106b92a3eb8bed14f276d416ebae870c6a5c
El Mehdi Adnani <86515202+MehdiAdd@users.noreply.github.com>
2022-06-29 20:42:46 +0100

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/OGiELMJsEunkJ7Q6P_qUVLyufbjq7kzL',
-      accounts: ['ee7d787dd31e858fcb3a5c74e9f7142ab57da8f68297a37709217eb208a23905'],
+      accounts: ['XXXXXXX'],
     },
   },
 };
